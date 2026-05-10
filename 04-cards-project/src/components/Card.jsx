6067029bee76bd62035ab038830a408b0f34149d
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-10 18:35:34 +0530

    added jobSaveButton

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import InstaLogo from "../assets/InstagramLogo.jpeg"
+import saveIcon from "../assets/SaveJobIcon.jpeg"
 
 const Card = () => {
   return (
     <div>
       <div className='card'>
         <img src={InstaLogo} className='logo'></img>
+        <button className='saveJobButton'>
+            Save
+            <img src={saveIcon} className='savelogo'></img>
+        </button>
       </div>
     </div>
   )
