4cd07265ad8ef529e2922ec44e5ddc7bb46cdc15
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-10 18:43:03 +0530

    job card structure halfway done

--- 04-cards-project/src/components/Card.jsx
+++ 04-cards-project/src/components/Card.jsx
@@ -6,11 +6,29 @@ const Card = () => {
   return (
     <div>
       <div className='card'>
-        <img src={InstaLogo} className='logo'></img>
-        <button className='saveJobButton'>
-            Save
-            <img src={saveIcon} className='savelogo'></img>
-        </button>
+
+        <div className='card-top'>
+            <img src={InstaLogo} className='logo'></img>
+            <button className='saveJobButton'>
+                Save
+                <img src={saveIcon} className='savelogo'></img>
+            </button>
+        </div>
+
+        <div className='card-middle'>
+            <h2 className='CompanyName'>Ammazon</h2>
+            <span className='JobPostTime'>5 days ago</span>
+            <h1>Senior UI/UX Designer</h1>
+            <span className='JobTags'>Part-Time</span>
+            <span className='JobTags'>Senior Level</span>
+        </div>
+
+        <div className='card-bottom'>
+            <hr></hr>
+            <span className='JobSalary'>$120/hr</span>
+            <button className='JobApplyButton'>Apply now</button>
+        </div>
+
       </div>
     </div>
   )
