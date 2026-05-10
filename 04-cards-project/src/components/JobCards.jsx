e4ff801041ec4dc7e2ecbcf2bbc5b04c4bb787c8
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-10 20:56:00 +0530

    added appropriate job-desc

--- 04-cards-project/src/components/JobCards.jsx
+++ 04-cards-project/src/components/JobCards.jsx
@@ -7,36 +7,32 @@ import pinterestLogo from '../assets/PinterestLogo.jpeg'
 import spotifyLogo from '../assets/SpotifyLogo.png'
 import telegramLogo from '../assets/TelegramLogo.jpeg'
 
-// Card parameter -> logo (img), CompanyName (string), postTime (string),
-// title (string), tags (arr), salary (string), location (string)
-
 const JobCards = () => {
   return (
     <div className='parent'>
-        <Card logo={appleLogo} companyName="Apple" postTime="5 days ago"
-            title="Sr. Software Engineer" salary="120$/hr" location="India, Bengaluru"
-            tags = {["Part-Time", "Senior Level", "5 Days a week"]}/>
+      <Card logo={appleLogo} companyName="Apple" postTime="5 days ago"
+        title="Sr. Software Engineer" salary="$140k/yr" location="India, Bengaluru"
+        tags={["Full-Time", "Senior Level"]}/>
 
-        <Card logo={googleLogo} companyName="Google" postTime="3 days ago"
-            title="Jr. Data Analyst" salary="15$/hr" location="India, Pune"
-            tags = {["Part-Time", "Senior Level", "5 Days a week"]}/>
+      <Card logo={googleLogo} companyName="Google" postTime="3 days ago"
+        title="Data Analyst" salary="$90k/yr" location="India, Hyderabad"
+        tags={["Full-Time", "Flexible Schedule"]}/>
 
-        <Card logo={instagramLogo} companyName="Instagram" postTime="1 week ago"
-            title="Jr. Software Developer" salary="120$/hr" location="India, Bengaluru"
-            tags = {["Part-Time", "Senior Level", "5 Days a week"]}/>
+      <Card logo={instagramLogo} companyName="Instagram" postTime="1 week ago"
+        title="UI/UX Designer" salary="$85/hr" location="India, Mumbai"
+        tags={["Contract", "Remote"]}/>
 
-        <Card logo={appleLogo} companyName="Apple" postTime="5 days ago"
-            title="Sr. Software Engineer" salary="120$/hr" location="India, Bengaluru"
-            tags = {["Part-Time", "Senior Level", "5 Days a week"]}/>
+      <Card logo={pinterestLogo} companyName="Pinterest" postTime="2 days ago"
+        title="Frontend Developer" salary="$100/hr" location="India, Pune"
+        tags={["Full-Time", "In Office"]}/>
 
-        <Card logo={appleLogo} companyName="Apple" postTime="5 days ago"
-            title="Sr. Software Engineer" salary="120$/hr" location="India, Bengaluru"
-            tags = {["Part-Time", "Senior Level", "5 Days a week"]}/>
+      <Card logo={spotifyLogo} companyName="Spotify" postTime="today"
+        title="React Developer" salary="$110k/yr" location="India, Delhi"
+        tags={["Part-Time", "Remote"]}/>
 
-        <Card logo={appleLogo} companyName="Apple" postTime="5 days ago"
-            title="Sr. Software Engineer" salary="120$/hr" location="India, Bengaluru"
-            tags = {["Part-Time", "Senior Level", "5 Days a week"]}/>
-
+      <Card logo={telegramLogo} companyName="Telegram" postTime="6 days ago"
+        title="Backend Engineer" salary="$95k/yr" location="India, Chennai"
+        tags={["Full-Time", "Senior Level"]}/>
     </div>
   )
 }
