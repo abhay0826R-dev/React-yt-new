a4d5cebb729d5feeab58e97300a500d22a7cc6e6
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-10 16:28:48 +0530

    learned about components

---
+++ 02-Components/src/components/Card.jsx
@@ -0,0 +1,8 @@
+function Card() {
+    return <div className='card'>
+      <h1>App</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In consequuntur necessitatibus explicabo quaerat recusandae aliquid placeat nobis magnam. Illo unde placeat vitae ullam molestiae numquam error ab porro doloremque blanditiis.</p>
+    </div>
+}
+
+export default Card
